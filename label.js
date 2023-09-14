0c1f9608c8a39274902ef0f1d3405df8544cce4c
Boy Baukema <bbaukema@veracode.com>
2023-09-14 11:03:11 +0200

    Switch from token to Bearer because that is compatible with all tokens: https://docs.github.com/en/rest/overview/authenticating-to-the-rest-api?apiVersion=2022-11-28#about-authentication

--- label.js
+++ label.js
@@ -69,8 +69,7 @@ async function createLabels(options) {
     // create label, accept error code if it already exists
     console.log('Creating VeracodeFlaw labels');
 
-    var authToken = 'token ' + githubToken;
-    console.log("TEKNO!")
+    var authToken = 'Bearer ' + githubToken;
 
     for(const element of flawLabels.concat(otherLabels) ) {
         await request('POST /repos/{owner}/{repo}/labels', {
